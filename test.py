b86c2c54088d73ccecee274fc6e77b85a07e87a5
Aadi Nashikkar <aadinashikkar@gmail.com>
2025-05-11 15:17:33 -0700

    initial

---
+++ test.py
@@ -0,0 +1,9 @@
+# get my imports
+
+# couple different tool calls
+# 1. load in the first few lines of a jsonl file
+# 2. execute python code
+# 3. validate the returned output
+
+# And then the agents have access to these tools
+# they can run other agents based on the outputs
